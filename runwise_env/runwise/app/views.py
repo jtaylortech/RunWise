defb6ccfcd49a9021bdae2bcbb9ad8c40a9ff831
Jarred Taylor <jarrede20@gmail.com>
2023-07-31 21:12:45 -0400

    implementing user registration and login

--- runwise_env/runwise/app/views.py
+++ runwise_env/runwise/app/views.py
@@ -3,28 +3,30 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from .forms import RegistrationForm, LoginForm
+
+# app/views.py
 
 def register_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate(username=username, password=password)
-            if user:
-                login(request, user)
-                return redirect('home')  # Replace 'home' with the URL of app's home page
+            return redirect('login')  # Redirect to the login page after successful registration
     else:
-        form = UserCreationForm()
+        form = RegistrationForm()
+
     return render(request, 'registration/register.html', {'form': form})
 
 def user_login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(username=username, password=password)
-        if user:
-            login(request, user)
-            return redirect('home')  # Replace 'home' with the URL of your app's home page
-    return render(request, 'registration/login.html')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # Handle login logic (e.g., check user credentials and authenticate user)
+            # After successful login, redirect to the user's profile or dashboard
+            return redirect('dashboard')  # Replace 'dashboard' with the URL name for the user's profile page
+    else:
+        form = LoginForm()
+
+    return render(request, 'registration/login.html', {'form': form})
+
