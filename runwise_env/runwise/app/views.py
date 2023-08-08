4cff4811faa295cc5d5863e9dd9b04ba0df85c14
Jarred Taylor <jarrede20@gmail.com>
2023-08-07 21:52:31 -0400

    implemented password hashing with djangos built in functionality

--- runwise_env/runwise/app/views.py
+++ runwise_env/runwise/app/views.py
@@ -3,6 +3,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.hashers import make_password
 from .forms import RegistrationForm, LoginForm
 
 # app/views.py
@@ -14,12 +15,19 @@ def register_user(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            # Has the user's password before saving
+            password = make_password(form.cleaned_data['password'])
+            # Create the user object with the hashed password
+            user = form.save(commit=False)
+            user.password = password
+            user.save()
             return redirect('login')  # Redirect to the login page after successful registration
     else:
         form = RegistrationForm()
 
     return render(request, 'registration/register.html', {'form': form})
+
+    
 
 def user_login(request):
     if request.method == 'POST':
